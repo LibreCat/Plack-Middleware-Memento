20e12e48c79f8c030f9b886c4d93b5251ad6cb65
Nicolas Steenlant <nicolas.steenlant@ugent.be>
2015-03-10 09:39:47 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# NAME
+
+Plack::Middleware::Memento - Blah blah blah
+
+# SYNOPSIS
+
+    use Plack::Middleware::Memento;
+
+# DESCRIPTION
+
+Plack::Middleware::Memento is
+
+# AUTHOR
+
+Nicolas Steenlant <nicolas.steenlant@ugent.be>
+
+# COPYRIGHT
+
+Copyright 2015- Nicolas Steenlant
+
+# LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# SEE ALSO
---
+++ lib/Plack/Middleware/Memento.pm
@@ -0,0 +1,39 @@
+package Plack::Middleware::Memento;
+
+use strict;
+use 5.008_005;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Plack::Middleware::Memento - Blah blah blah
+
+=head1 SYNOPSIS
+
+  use Plack::Middleware::Memento;
+
+=head1 DESCRIPTION
+
+Plack::Middleware::Memento is
+
+=head1 AUTHOR
+
+Nicolas Steenlant E<lt>nicolas.steenlant@ugent.beE<gt>
+
+=head1 COPYRIGHT
+
+Copyright 2015- Nicolas Steenlant
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 SEE ALSO
+
+=cut
