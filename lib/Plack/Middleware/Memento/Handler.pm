e81531e7742713e0fcedf41386c039e5ef2ce021
Nicolas Steenlant <nicolas.steenlant@ugent.be>
2015-03-12 14:39:36 +0100

    refactor

--- lib/Plack/Middleware/Memento/Handler.pm
+++ lib/Plack/Middleware/Memento/Handler.pm
@@ -5,7 +5,6 @@ use warnings;
 use Moo::Role;
 
 requires 'get_memento';
-
 requires 'get_all_mementos';
 
 1;
--- lib/Plack/Middleware/Memento/TimeGate.pm
+++ lib/Plack/Middleware/Memento/TimeGate.pm
@@ -4,22 +4,28 @@ use strict;
 use warnings;
 use Plack::Request;
 use Plack::Util ();
-use Plack::Util::Accessor qw(path handler options);
 use parent qw(Plack::Middleware);
+use Plack::Util::Accessor qw(
+    timegate_path
+    timemap_path
+    handler
+    options
+);
 
 sub call {
     my($self, $env) = @_;
-    my $res = $self->_handle($env);
+    my $res = $self->_handle_timegate_request($env) ||
+        $self->_handle_timemap_request($env);
     if ($res) {
       return $res;
     }
     $self->app->($env);
 }
 
-sub _handle {
+sub _handle_timegate_request {
     my($self, $env) = @_;
 
-    my $path_match = $self->path or return;
+    my $path_match = $self->timegate_path or return;
     my $uri = $env->{PATH_INFO};
 
     $uri =~ s/$path_match// or return;
@@ -27,9 +33,10 @@ sub _handle {
     my $req = Plack::Request->new($env);
     my $datetime = $req->header('Accept-Datetime');
 
-    my $memento_uri = $self->_handler->get_memento($uri, $datetime) || return $self->_not_found;    
+    my $memento_uri = $self->_handler->get_memento($uri, $datetime) ||
+        return $self->_handle_not_found;    
 
-    [ 302,
+    return [ 302,
         [
             'Vary' => 'accept-datetime',
             'Location' => $memento_uri,
@@ -37,6 +44,28 @@ sub _handle {
             'Link' => qq|<$uri>; rel="original",<>; rel="timegate", <>; rel="timemap"|,
         ],
         [ ],
+    ];
+}
+
+sub _handle_timemap_request {
+    my($self, $env) = @_;
+
+    my $path_match = $self->timemap_path or return;
+    my $uri = $env->{PATH_INFO};
+
+    $uri =~ s/$path_match// or return;
+
+    my $mementos = $self->_handler->get_all_mementos($uri);
+
+    [ 200,
+        [
+            'Content-Type' => 'application/link-format',
+        ],
+        [   
+            qq|<$uri>; rel="original",\n|,
+            qq|<>; rel="self"; type="application/link-format",\n|,
+            $self->_to_link_format(@$mementos),
+        ],
     ];
 }
 
@@ -48,14 +77,18 @@ sub _handler {
     };
 }
 
-sub _not_found {
+sub _handle_not_found {
     my($self) = @_;
-    [ 404,
-        [
-            'Content-Type' => 'text/plain; charset=UTF-8',
-        ],
-        [ ],
-    ];
+    [ 404, [ 'Content-Type' => 'text/plain; charset=UTF-8' ], [] ];
+}
+
+sub _to_link_format {
+    my ($self, @mementos) = @_;
+    my $body = join(",\n", map {
+        my ($uri, $datetime) = @$_;
+        qq|<$uri>; rel="memento"; datetime="$datetime"|;
+    } @mementos);
+    "$body\n";
 }
 
 1;
