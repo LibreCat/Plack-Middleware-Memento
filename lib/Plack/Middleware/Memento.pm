cde211b8a2745d2d167863b29d48e6c9fd514135
Nicolas Steenlant <nicolas.steenlant@ugent.be>
2017-05-19 12:00:01 +0200

    tidy

--- lib/Plack/Middleware/Memento.pm
+++ lib/Plack/Middleware/Memento.pm
@@ -25,7 +25,9 @@ sub _handler_options {
     $self->{_handler_options} ||= do {
         my $options = {};
         for my $key (keys %$self) {
-            next if $key =~ /(?:^_)|(?:^(?:handler|timegate_path|timemap_path)$)/;
+            next
+                if $key
+                =~ /(?:^_)|(?:^(?:handler|timegate_path|timemap_path)$)/;
             $options->{$key} = $self->{$key};
         }
         $options;
@@ -35,16 +37,17 @@ sub _handler_options {
 sub _handler {
     my ($self) = @_;
     $self->{_handler} ||= do {
-        my $class = Plack::Util::load_class($self->{handler}, 'Plack::Middleware::Memento::Handler');
+        my $class = Plack::Util::load_class($self->{handler},
+            'Plack::Middleware::Memento::Handler');
         $class->new($self->_handler_options);
     };
 }
 
 sub call {
     my ($self, $env) = @_;
-    $self->_handle_timegate_request($env) ||
-        $self->_handle_timemap_request($env) ||
-        $self->_wrap_request($env);
+    $self->_handle_timegate_request($env)
+        || $self->_handle_timemap_request($env)
+        || $self->_wrap_request($env);
 }
 
 sub _wrap_request {
@@ -57,11 +60,13 @@ sub _wrap_request {
             $self->_timegate_link($req->base, $uri_r),
             $self->_timemap_link($req->base, $uri_r, 'timemap'),
         );
-        Plack::Util::header_set($res->[1], 'Memento-Datetime', DateTime::Format::HTTP->format_datetime($dt));
+        Plack::Util::header_set($res->[1], 'Memento-Datetime',
+            DateTime::Format::HTTP->format_datetime($dt));
         Plack::Util::header_push($res->[1], 'Link', join(",", @links));
     }
     if ($self->_handler->wrap_original_resource_request($req)) {
-        Plack::Util::header_push($res->[1], 'Link', $self->_timegate_link($req->base, $req->uri->as_string));
+        Plack::Util::header_push($res->[1], 'Link',
+            $self->_timegate_link($req->base, $req->uri->as_string));
     }
     $res;
 }
@@ -70,29 +75,30 @@ sub _handle_timegate_request {
     my ($self, $env) = @_;
 
     my $prefix = $self->timegate_path;
-    my $uri_r = $env->{PATH_INFO};
+    my $uri_r  = $env->{PATH_INFO};
     $uri_r =~ s|^${prefix}/|| or return;
 
     my $req = Plack::Request->new($env);
 
-    my $mementos = $self->_handler->get_all_mementos($uri_r, $req) ||
-        return $self->_not_found;
+    my $mementos = $self->_handler->get_all_mementos($uri_r, $req)
+        || return $self->_not_found;
 
-    $mementos = [sort { DateTime->compare($a->[1], $b->[1]) } @$mementos];
+    $mementos = [sort {DateTime->compare($a->[1], $b->[1])} @$mementos];
 
     my $closest_mem;
 
     if (defined(my $date = $req->header('Accept-Datetime'))) {
-        my $dt = eval { DateTime::Format::HTTP->parse_datetime($date) } or
-            return $self->_bad_request;
+        my $dt = eval {DateTime::Format::HTTP->parse_datetime($date)}
+            or return $self->_bad_request;
 
-        my ($closest) = sort { $a->[1] <=> $b->[1] } map {
+        my ($closest) = sort {$a->[1] <=> $b->[1]} map {
             my $diff = abs($_->[1]->epoch - $dt->epoch);
             [$_, $diff];
         } @$mementos;
 
         $closest_mem = $closest->[0];
-    } else {
+    }
+    else {
         $closest_mem = $mementos->[-1];
     }
 
@@ -103,26 +109,30 @@ sub _handle_timegate_request {
 
     if (@$mementos == 1) {
         push @links, $self->_memento_link($closest_mem, 'first last memento');
-    } elsif ($closest_mem->[0] eq $mementos->[0]->[0]) {
-        push @links, $self->_memento_link($closest_mem, 'first memento');
+    }
+    elsif ($closest_mem->[0] eq $mementos->[0]->[0]) {
+        push @links, $self->_memento_link($closest_mem,    'first memento');
         push @links, $self->_memento_link($mementos->[-1], 'last memento');
-    } elsif ($closest_mem->[0] eq $mementos->[-1]->[0]) {
+    }
+    elsif ($closest_mem->[0] eq $mementos->[-1]->[0]) {
         push @links, $self->_memento_link($mementos->[0], 'first memento');
-        push @links, $self->_memento_link($closest_mem, 'last memento');
-    } else {
-        push @links, $self->_memento_link($mementos->[0], 'first memento');
-        push @links, $self->_memento_link($closest_mem, 'memento');
+        push @links, $self->_memento_link($closest_mem,   'last memento');
+    }
+    else {
+        push @links, $self->_memento_link($mementos->[0],  'first memento');
+        push @links, $self->_memento_link($closest_mem,    'memento');
         push @links, $self->_memento_link($mementos->[-1], 'last memento');
     }
 
-    [ 302,
+    [
+        302,
         [
-            'Vary' => 'accept-datetime',
-            'Location' => $closest_mem->[0],
+            'Vary'         => 'accept-datetime',
+            'Location'     => $closest_mem->[0],
             'Content-Type' => 'text/plain; charset=UTF-8',
-            'Link' => join(",", @links),
+            'Link'         => join(",", @links),
         ],
-        [ ],
+        [],
     ];
 }
 
@@ -130,15 +140,15 @@ sub _handle_timemap_request {
     my ($self, $env) = @_;
 
     my $prefix = $self->timemap_path;
-    my $uri_r = $env->{PATH_INFO};
+    my $uri_r  = $env->{PATH_INFO};
     $uri_r =~ s|^${prefix}/|| or return;
 
     my $req = Plack::Request->new($env);
 
-    my $mementos = $self->_handler->get_all_mementos($uri_r, $req) ||
-        return $self->_not_found;
+    my $mementos = $self->_handler->get_all_mementos($uri_r, $req)
+        || return $self->_not_found;
 
-    $mementos = [sort { DateTime->compare($a->[1], $b->[1]) } @$mementos];
+    $mementos = [sort {DateTime->compare($a->[1], $b->[1])} @$mementos];
 
     my @links = (
         $self->_original_link($uri_r),
@@ -147,35 +157,34 @@ sub _handle_timemap_request {
     );
 
     if (@$mementos == 1) {
-        push @links, $self->_memento_link($mementos->[0], 'first last memento');
-    } else {
+        push @links,
+            $self->_memento_link($mementos->[0], 'first last memento');
+    }
+    else {
         if (my $first_mem = shift @$mementos) {
             push @links, $self->_memento_link($first_mem, 'first memento');
         }
         if (my $last_mem = pop @$mementos) {
             push @links, $self->_memento_link($last_mem, 'last memento');
         }
-        push @links, map { $self->_memento_link($_, 'memento') } @$mementos;
+        push @links, map {$self->_memento_link($_, 'memento')} @$mementos;
     }
 
-    [ 200,
-        [
-            'Content-Type' => 'application/link-format',
-        ],
-        [
-            join(",\n", @links),
-        ],
+    [
+        200,
+        ['Content-Type' => 'application/link-format',],
+        [join(",\n", @links),],
     ];
 }
 
 sub _not_found {
     my ($self) = @_;
-    [ 404, [ 'Content-Type' => 'text/plain; charset=UTF-8' ], [] ];
+    [404, ['Content-Type' => 'text/plain; charset=UTF-8'], []];
 }
 
 sub _bad_request {
     my ($self) = @_;
-    [ 400, [ 'Content-Type' => 'text/plain; charset=UTF-8' ], [] ];
+    [400, ['Content-Type' => 'text/plain; charset=UTF-8'], []];
 }
 
 sub _original_link {
@@ -187,10 +196,12 @@ sub _timemap_link {
     my ($self, $base_url, $uri_r, $rel, $mementos) = @_;
     $base_url->path(join('/', $self->timemap_path, $uri_r));
     my $uri_t = $base_url->canonical->as_string;
-    my $link = qq|<$uri_t>; rel="$rel"; type="application/link-format"|;
+    my $link  = qq|<$uri_t>; rel="$rel"; type="application/link-format"|;
     if ($mementos) {
-        my $from = DateTime::Format::HTTP->format_datetime($mementos->[0]->[1]);
-        my $until = DateTime::Format::HTTP->format_datetime($mementos->[-1]->[1]);
+        my $from
+            = DateTime::Format::HTTP->format_datetime($mementos->[0]->[1]);
+        my $until
+            = DateTime::Format::HTTP->format_datetime($mementos->[-1]->[1]);
         $link .= qq|; from="$from"; until="$until"|;
     }
     $link;
@@ -205,7 +216,7 @@ sub _timegate_link {
 
 sub _memento_link {
     my ($self, $mem, $rel) = @_;
-    my $uri_m = $mem->[0];
+    my $uri_m    = $mem->[0];
     my $datetime = DateTime::Format::HTTP->format_datetime($mem->[1]);
     qq|<$uri_m>; rel="$rel"; datetime="$datetime"|;
 }
