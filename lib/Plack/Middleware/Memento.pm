a7f55701075c8c4d7b9ae304b62043202db41a69
Nicolas Steenlant <nicolas.steenlant@ugent.be>
2017-05-19 11:46:38 +0200

    minimal pod

--- lib/Plack/Middleware/Memento.pm
+++ lib/Plack/Middleware/Memento.pm
@@ -212,3 +212,31 @@ sub _memento_link {
 
 1;
 
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Plack::Middleware::Memento::Handler - Base role and interface for Plack::Middleware::Memento handlers
+
+=head1 DESCRIPTION
+
+This is an early minimal release, documentation and tests are lacking.
+
+=head1 AUTHOR
+
+Nicolas Steenlant E<lt>nicolas.steenlant@ugent.beE<gt>
+
+=head1 COPYRIGHT
+
+Copyright 2017- Nicolas Steenlant
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 SEE ALSO
+
+=cut
--- lib/Plack/Middleware/Memento/Handler.pm
+++ lib/Plack/Middleware/Memento/Handler.pm
@@ -18,3 +18,45 @@ sub wrap_memento_request {
 }
 
 1;
+
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Plack::Middleware::Memento - Enable the Memento protocol
+
+=head1 SYNOPSIS
+
+    use Plack::Builder;
+    use Plack::App::Catmandu::Bag;
+
+    builder {
+        enable 'Memento', handler => 'Catmandu::Bag', store => 'authority', bag => 'person';
+        Plack::App::Catmandu::Bag->new(
+            store => 'authority',
+            bag => 'person',
+        )->to_app;
+    };
+
+=head1 DESCRIPTION
+
+This is an early minimal release, documentation and tests are lacking.
+
+=head1 AUTHOR
+
+Nicolas Steenlant E<lt>nicolas.steenlant@ugent.beE<gt>
+
+=head1 COPYRIGHT
+
+Copyright 2017- Nicolas Steenlant
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 SEE ALSO
+
+=cut
